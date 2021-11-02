37a0e729e1b78a9c34037c5bf63c7699bd89343b
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-11-02 09:48:34 -0500

    finished front-end

---
+++ book-cataloger/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+const mongoose = require('mongoose');
+
+
+author: String,
+title: {type:String, require: true},
+genre: String
+
+
+function App() {
+  return (
+    assName="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/App.js
@@ -0,0 +1,139 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+
+author: String,
+title: {type:String, require: true},
+genre: String
+
+/books
+
+function App() {
+
+  const [newAuthor, setAuthor] = useState('');
+  const [newTitle, setTitle] = useState('');
+  const [newGenre, setGenre] = useState('');
+  const [books, setBooks] = useState('');
+
+  const handleNewAuthor = (event) => {
+    setAuthor(event.target.value)
+  }
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleNewGenre = (event) => {
+    setGenre(event.target.value)
+  }
+
+  const handleNewBookFormSubmit = (event) => {
+    event.preventDefault();
+        axios.post(
+            'http://localhost:3000/books',
+            {
+                author: newAuthor,
+                title: newTitle,
+                genre: newGenre
+
+            }
+        ).then(()=>{
+            axios
+                .get('http://localhost:3000/books')
+                .then((response)=>{
+                    setBooks(response.data)
+                })
+            })
+  }
+
+  const handleBookUpdate = (event) => {
+    axios
+      .put(
+          `http://localhost:3000/books/${books._id}`,
+          {
+              author: newAuthor,
+              title: newTitle,
+              genre: newGenre
+          }
+      )
+      .then(()=>{
+          axios
+              .get('http://localhost:3000/books')
+              .then((response)=>{
+                  setBooks(response.data)
+              })
+      })
+  }
+
+  const handleDelete = (bookData) => {
+    axios
+      .delete(`http://localhost:3000/books/${bookData._id}`)
+      .then(() =>{
+        axios
+          .get(`http://localhost:3000/books`)
+          .then((response) => {
+            setBooks(response.data)
+          })
+      })
+  }
+
+  useEffect(()=>{
+      axios
+          .get('http://localhost:3000/books')
+          .then((response)=>{
+              setBooks(response.data)
+          })
+  },[])
+
+
+
+
+
+  return (
+    <main>
+      <h1>Book Listing</h1>
+        <section>
+          <h2>Create a new Book</h2>
+          <form onSubmit={handleNewBookFormSubmit}>
+            Author: <input type="text" onChange={handleNewGenre}/><br/>
+            Title: <input type="text" onChange={handleTitleChange} /><br/>
+            Genre: <input type="text" onChange={handleNewGenre} /><br/>
+            <input type="submit" value="create animal"/>
+          </form>
+        </section>
+        <section>
+          <h2>Books</h2>
+          <ul>
+            {
+              books.map((book) =>{
+                return <li>
+                        Author: {book.author}
+                        <br/>
+                        Title: {book.title}
+                        <br/>
+                        Genre: {book.genre}
+                        <br/>
+                        <section>
+                          <h2>Edit Book</h2>
+                          <form onSubmit={handleBookUpdate}>
+                            Author: <input type="text" onChange={handleNewAuthor}/>
+                            <br/>
+                            Title: <input type="text" onSubmit={handleTitleChange} />
+                            <br/>
+                            Genre: <input type="text" onSubmit={handleNewGenre} />
+                            <br/>
+                            <input type="submit" value="Submit Edits" />
+                          </form>
+                        </section>
+                      <button onClick={ (event)=>{handleDelete(book)}}>Bought!</button>
+                      </li>
+                        })
+            }
+          </ul>
+        </section>
+    </main>
+  );
+}
+
+export default App;
